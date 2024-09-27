8791f8aa790b1215e228142056fa9eaba403661e
23DP4BAlek <166509145+23DP4BAlek@users.noreply.github.com>
2024-09-27 09:42:17 +0000

    Speeding ticket

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,19 +6,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Give the first number:");
-        int first = Integer.valueOf(scanner.nextLine());
-        System.out.println("Give the second number:");
-        int second = Integer.valueOf(scanner.nextLine());
-        int result1 = first * second;
-        double result = 1.0 * first / second;
-        int result2 = first + second;
-        int result3 = first - second;
-        System.out.println(first + "+" + second + "=" + result2);
-        System.out.println(first + "-" + second + "=" + result3);
-        System.out.println(first + "*" + second + "=" + result1);
-        System.out.println(first + "/" + second + "=" + result);
+        
+    
+            Scanner scanner = new Scanner(System.in);
+            int value = Integer.valueOf(scanner.nextLine());
+            
+            if (value > 120) {
+            System.out.println("Speeding ticket!");
+        }
   // Make your Hello World programm!
     }
 }
