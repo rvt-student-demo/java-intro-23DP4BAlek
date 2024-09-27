60e5aa541c3f5cd7892ba02e462f46782c190e88
23DP4BAlek <166509145+23DP4BAlek@users.noreply.github.com>
2024-09-27 10:54:04 +0000

    Grades and Points

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -11,9 +11,24 @@ public class App
             Scanner scanner = new Scanner(System.in);
             int value = Integer.valueOf(scanner.nextLine());
             
-            if (value > 120) {
-            System.out.println("Speeding ticket!");
-        }
+            if (value < 0) {
+                System.out.println("impossible");
+            } else if (value <= 49) {
+                System.out.println("failed");
+            } else if (value <= 59) {  
+                System.out.println("1");
+            } else if (value <= 69) {
+                System.out.println("2");
+            } else if (value <= 79) {
+                System.out.println("3"); 
+            } else if (value <= 89) {
+                System.out.println("4");
+            } else if (value <= 100) {
+                System.out.println("5");
+            } else if (value > 100); {
+                System.out.println("Incredible!");
+            }
+        
   // Make your Hello World programm!
     }
 }
