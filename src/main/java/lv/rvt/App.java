b9ca0b5cbd9851d6fcb03c58fb26051667a892b5
23DP4BAlek <166509145+23DP4BAlek@users.noreply.github.com>
2024-10-25 11:05:00 +0000

    christmas tree

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,37 +4,42 @@ import java.util.Scanner;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
-        
     
-            Scanner scanner = new Scanner(System.in);
 
-            Integer value = Integer.valueOf(scanner.nextLine());
+        
+        public static void christmasTree(int height) {
             
-            if (value <= 5000) {
-                System.out.println("No tax!");
-            } else if (value <= 25000) {
-                double result = (100 + (value - 5000) * 0.08); 
-                double resultAsD = (double) result;
-                System.out.println(resultAsD);
-            } else if (value <= 55000) {
-                double result = (1700 + (value - 25000) * 0.1); 
-                double resultAsD = (double) result;
-                System.out.println(resultAsD);
-            } else if (value <= 200000) {
-                double result = (4700 + (value - 55000) * 0.12);
-                double resultAsD = (double) result;
-            System.out.println(resultAsD);
-            } else if (value <= 1000000) {
-                double result = (22100 + (value - 200000) * 0.15);
-                double resultAsD = (double) result;
-            System.out.println(resultAsD);
-            } else if (value > 1000000) {
-                double result = (142100 + (value - 1000000) * 0.17);
-                double resultAsD = (double) result;
-            System.out.println(resultAsD);
+            for (int i = 1; i <= height; i++) {
+                printSpaces(height - i);
+                printStars(2 * i + 1);
+                System.out.println();
             }
-  // Make your Hello World programm!
+    
+            
+            for (int i = 0; i < 2; i++) {
+                printSpaces(height - 1);
+                printStars(3);
+                System.out.println();
+            }
+        }
+    
+        
+        private static void printSpaces(int count) {
+            for (int i = 0; i < count; i++) {
+                System.out.print(" ");
+            }
+        }
+    
+        
+        private static void printStars(int count) {
+            for (int i = 0; i < count; i++) {
+                System.out.print("*");
+            }
+        }
+    
+        
+        public static void main(String[] args) {
+            christmasTree(15);  
+        
     }
 }
