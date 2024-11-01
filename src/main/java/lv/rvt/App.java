98359b60d01519bd648173782f48ce8094ea9d67
23DP4BAlek <166509145+23DP4BAlek@users.noreply.github.com>
2024-11-01 12:26:43 +0000

    Personal Details

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,35 +1,42 @@
 package lv.rvt;
 
-import java.util.Arrays;
+
 import java.util.Scanner;
 
 public class App 
-{
-    
-
-        
+{        
         public static void main( String[] args ) {
-            int[] val0 = {0, 1, 2, 3}; 
-
+            Scanner reader = new Scanner(System.in);
             int sum = 0;
-            for(int i = 0; i < val0.length; i++) {
-                sum += val0[i];
-            } 
-            System.out.println( "Sum of all numbers = " + sum );
+            int count = 0;
+            String longestName = "";
             
-            int[] val = {13, -4, 82, 17}; 
-            int[] twice;
-            System.out.println( "Original Array: " 
-            + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
-            twice = new int[val.length];
-            for (int i = 0; i < val.length; i++) {
-                twice[i] = 2 * val[i];
+            while (true) {
+                String input = reader.nextLine();
+                if (input.equals("")) {
+                    break;
+                }
+    
+                String[] parts = input.split(",");
 
+                int age;
+                    age = Integer.parseInt(parts[1].trim());
+
+                
+                sum += age;
+                count++;
+                
+
+                String name = parts[0].trim();
+                if (name.length() > longestName.length()) {
+                    longestName = name;
+                }
             }
-
-            System.out.println(
-                Arrays.toString(twice)
-            );
-
+            
+            if (count > 0) {
+                System.out.println("Longest name: " + longestName + ", Date of birth average: " + (1.0 * sum / count));
+            } else {
+                System.out.println("No input.");
+            }
+                }
     }
-}
