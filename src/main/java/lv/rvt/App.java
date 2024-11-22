18fb553c87230f96f782a21f218b5ae07f805351
23DP4BAlek <166509145+23DP4BAlek@users.noreply.github.com>
2024-11-22 12:18:24 +0000

    timer

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,42 +1,25 @@
 package lv.rvt;
 
-
 import java.util.Scanner;
 
-public class App 
-{        
-        public static void main( String[] args ) {
-            Scanner reader = new Scanner(System.in);
-            int sum = 0;
-            int count = 0;
-            String longestName = "";
-            
-            while (true) {
-                String input = reader.nextLine();
-                if (input.equals("")) {
-                    break;
-                }
-    
-                String[] parts = input.split(",");
 
-                int age;
-                    age = Integer.parseInt(parts[1].trim());
+public class App {
 
-                
-                sum += age;
-                count++;
-                
+    public static void main(String[] args) {
+       
+        Timer timer = new Timer();
 
-                String name = parts[0].trim();
-                if (name.length() > longestName.length()) {
-                    longestName = name;
-                }
+        while (true) {
+            System.out.println(timer);
+            timer.advance();
+
+            try {
+                Thread.sleep(10);
+            } catch (Exception e) {
+
             }
-            
-            if (count > 0) {
-                System.out.println("Longest name: " + longestName + ", Date of birth average: " + (1.0 * sum / count));
-            } else {
-                System.out.println("No input.");
-            }
-                }
+        }
+
     }
+}
+
