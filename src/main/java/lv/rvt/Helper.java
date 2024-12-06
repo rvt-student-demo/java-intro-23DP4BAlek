7341390cbd34f211299c5aea4ceb67e571be9c43
23DP4BAlek <166509145+23DP4BAlek@users.noreply.github.com>
2024-12-06 07:12:59 +0000

    project fix

--- src/main/java/lv/rvt/Helper.java
+++ src/main/java/lv/rvt/Helper.java
@@ -12,16 +12,16 @@ import java.nio.file.StandardOpenOption;
 
 public class Helper {
     public static BufferedReader getReader(String filename) throws IOException {
-        return File.newBufferedReader(getFilePath(filename), option);
+        return Files.newBufferedReader(getFilePath(filename));
     }
 
     public static BufferedWriter getWriter(String filename, StandardOpenOption option) throws IOException {
-        return Files.newBufferedWriter(getFilePath(filename), option)
+        return Files.newBufferedWriter(getFilePath(filename), option);
     }
 
     private static Path getFilePath(String filename) throws FileNotFoundException {
         Path filePath = Paths.get("data", filename);
-        if (!Files.eists(filePath)) {
+        if (!Files.exists(filePath)) {
             throw new FileNotFoundException("File not found: " + filename);
         }
         return filePath;
